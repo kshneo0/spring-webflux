82cd1d32cb653b04e50bd0ff1f179e8912640bf5
kshneo0 <kshneo@gmail.com>
2022-04-07 20:56:40 +0900

    9. Simple Math Service - DTO

---
+++ webflux-demo/src/main/java/com/ksh/webfluxdemo/dto/Response.java
@@ -0,0 +1,23 @@
+package com.ksh.webfluxdemo.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Response {
+	
+	private Date date = new Date();
+	private int output;
+	
+	public Response(int output) {
+		super();
+		this.output = output;
+	}
+	
+	
+	
+	
+}
