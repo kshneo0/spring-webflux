576e54c497c62dbb64d2fd24591f977270b7932e
kshneo0 <kshneo@gmail.com>
2022-04-08 06:18:01 +0900

    16. What happens when subscriber cancels

--- webflux-demo/src/main/java/com/ksh/webfluxdemo/service/ReactiveMathService.java
+++ webflux-demo/src/main/java/com/ksh/webfluxdemo/service/ReactiveMathService.java
@@ -1,6 +1,6 @@
 package com.ksh.webfluxdemo.service;
 
-import java.util.stream.Collectors;
+import java.time.Duration;
 
 import org.springframework.stereotype.Service;
 
@@ -19,7 +19,8 @@ public class ReactiveMathService {
 	
 	public Flux<Response> multiplicationTable(int input){
 		return Flux.range(1,10)
-				.doOnNext(i -> SleepUtil.sleepSeconds(1))
+				.delayElements(Duration.ofSeconds(1))
+//				.doOnNext(i -> SleepUtil.sleepSeconds(1))
 				.doOnNext(i-> System.out.println("math-service processing : " + i))
 				.map(i -> new Response(i * input));
 	}
