74adc34e36fcf6dceb26e3b29385a72d6c7cd254
kshneo0 <kshneo@gmail.com>
2022-04-08 21:23:26 +0900

    20. Request Header

--- webflux-demo/src/main/java/com/ksh/webfluxdemo/controller/ReactiveMathController.java
+++ webflux-demo/src/main/java/com/ksh/webfluxdemo/controller/ReactiveMathController.java
@@ -1,11 +1,14 @@
 package com.ksh.webfluxdemo.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,9 +42,17 @@ public class ReactiveMathController {
 	public Flux<Response> multiplicationTableStream(@PathVariable int input){
 		return this.mathService.multiplicationTable(input);
 	}
-	
+/*	
 	@PostMapping("multiply")
 	public Mono<Response> multiply(@RequestBody Mono<MultiplyRequestDto> requestDtoMono){
+		return this.mathService.multiply(requestDtoMono);
+		
+	}
+*/
+	@PostMapping("multiply")
+	public Mono<Response> multiply(@RequestBody Mono<MultiplyRequestDto> requestDtoMono,
+								   @RequestHeader Map<String, String> headers){
+		System.out.println(headers);
 		return this.mathService.multiply(requestDtoMono);
 		
 	}
