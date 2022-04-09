2faab04bdd679b78b4826adf8dd5c30abea6a296
kshneo0 <kshneo@gmail.com>
2022-04-09 10:55:53 +0900

    38. Assignment Solution

---
+++ webflux-demo/src/main/java/com/ksh/webfluxdemo/config/CalculatorRouterConfig.java
@@ -0,0 +1,42 @@
+package com.ksh.webfluxdemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class CalculatorRouterConfig {
+
+	@Autowired
+	private CalculatorHandler handler;
+	
+	@Bean
+	public RouterFunction<ServerResponse> highLevelCalculatorRouter() {
+		return RouterFunctions.route()
+				.path("calculator", this::serverResponseRouterFunction)
+				.build();
+	}
+	
+	private RouterFunction<ServerResponse> serverResponseRouterFunction() {
+		
+		return RouterFunctions.route()
+				.GET("{a}/{b}", isOperation("+"),handler::additionHandler)
+				.GET("{a}/{b}", isOperation("-"),handler::subtractionHandler)
+				.GET("{a}/{b}", isOperation("*"),handler::multiplicationHandler)
+				.GET("{a}/{b}", isOperation("/"),handler::divisionHandler)
+				.GET("{a}/{b}", req -> ServerResponse.badRequest().bodyValue("Op should be + - * /"))
+				.build();
+	}
+	
+	private RequestPredicate isOperation(String operation) {
+		return RequestPredicates.headers(headers -> operation.equals(headers.asHttpHeaders()
+														  .toSingleValueMap()
+														  .get("OP")));
+					
+	}
+}
