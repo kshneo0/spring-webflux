1a3aebe3fcae0217f98abef1ea529502c6dd9f26
kshneo0 <kshneo@gmail.com>
2022-04-09 09:49:48 +0900

    34. Exception Handling

--- webflux-demo/src/main/java/com/ksh/webfluxdemo/config/RequestHandler.java
+++ webflux-demo/src/main/java/com/ksh/webfluxdemo/config/RequestHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.ksh.webfluxdemo.dto.MultiplyRequestDto;
 import com.ksh.webfluxdemo.dto.Response;
+import com.ksh.webfluxdemo.exception.InputValidationException;
 import com.ksh.webfluxdemo.service.ReactiveMathService;
 
 import reactor.core.publisher.Flux;
@@ -45,6 +46,17 @@ public class RequestHandler {
 		
 		return ServerResponse.ok()
 				.body(responseMono, Response.class);
+	}
+	
+	public Mono<ServerResponse> squareHandlerWithValidation(ServerRequest serverRequest){
+		int input = Integer.valueOf(serverRequest.pathVariable("input"));
+		if(input < 10 || input > 20) {
+//			InputFailedValidationResponse response = new InputFailedValidationResponse();
+//			return ServerResponse.badRequest().bodyValue(response);
+			return Mono.error(new InputValidationException(input));
+		}
+		Mono<Response> responseMono = this.mathService.findSquare(input);
+		return ServerResponse.ok().body(responseMono, Response.class);
 	}
 
 }
--- webflux-demo/src/main/java/com/ksh/webfluxdemo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/ksh/webfluxdemo/config/RouterConfig.java
@@ -1,11 +1,19 @@
 package com.ksh.webfluxdemo.config;
 
+import java.util.function.BiFunction;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.ksh.webfluxdemo.dto.InputFailedValidationResponse;
+import com.ksh.webfluxdemo.exception.InputValidationException;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class RouterConfig {
@@ -20,6 +28,19 @@ public class RouterConfig {
 				.GET("router/table/{input}",requestHandler::tableHandler)
 				.GET("router/table/{input}/stream",requestHandler::tableStreamHandler)
 				.POST("router/multiply",requestHandler::multiplyHandler)
+				.GET("router/square/{input}/validation",requestHandler::squareHandlerWithValidation)
+				.onError(InputValidationException.class, exceptionHandler())
 				.build();
+	}
+	
+	private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler(){
+		return (err, req) -> {
+			InputValidationException ex = (InputValidationException)err;
+			InputFailedValidationResponse response  = new InputFailedValidationResponse();
+			response.setInput(ex.getInput());
+			response.setMessage(ex.getMessage());
+			response.setErrorCode(ex.getErrorCode());
+			return ServerResponse.badRequest().bodyValue(response);
+		};
 	}
 }
