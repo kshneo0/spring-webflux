64bc480a1a574ee3b03b3ce3a5a83b22ffec857e
kshneo0 <kshneo@gmail.com>
2022-04-09 21:34:47 +0900

    65. [Product-Service] - Service Layer - Part 1

---
+++ product-service/src/main/java/com/ksh/productservice/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.ksh.productservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ksh.productservice.dto.ProductDto;
+import com.ksh.productservice.repository.ProductRepository;
+import com.ksh.productservice.util.EntityDtoUtil;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public Flux<ProductDto> getAll() {
+		return this.repository.findAll()
+			.map(EntityDtoUtil::toDto);
+	}
+	
+	public Mono<ProductDto> getProductById(String id) {
+		return this.repository.findById(id)
+				.map(EntityDtoUtil::toDto);
+	}
+	
+	public Mono<ProductDto> insertProduct(Mono<ProductDto> productDtoMono) {
+		return productDtoMono
+			.map(EntityDtoUtil::toEntity)
+			.flatMap(this.repository::insert)
+			.map(EntityDtoUtil::toDto);
+	}
+
+}
