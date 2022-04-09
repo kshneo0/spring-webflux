a92397592877dc085d5b2f22bfb42ee7d977c82e
kshneo0 <kshneo@gmail.com>
2022-04-09 17:06:10 +0900

    52. Setting Auth Token

--- webflux-demo/src/test/java/com/ksh/webfluxdemo/Lec04HeadersTest.java
+++ webflux-demo/src/test/java/com/ksh/webfluxdemo/Lec04HeadersTest.java
@@ -22,9 +22,14 @@ public class Lec04HeadersTest extends BaseTest {
 			.uri("reactive-math/multiply")
 			.bodyValue(buildRequestDto(5,2))
 			.headers(h -> h.set("someKey","someVal"))
+//			.headers(h -> h.setBasicAuth("username","password"))
 			.retrieve()
 			.bodyToMono(Response.class)
 			.doOnNext(System.out::println);
+		
+		StepVerifier.create(responseMono)
+		.expectNextCount(1)
+		.verifyComplete();
 		
 		StepVerifier.create(responseMono)
 		.expectNextCount(1)
--- webflux-demo/src/test/java/com/ksh/webfluxdemo/config/WebClientConfig.java
+++ webflux-demo/src/test/java/com/ksh/webfluxdemo/config/WebClientConfig.java
@@ -2,15 +2,30 @@ package com.ksh.webfluxdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
 
 	@Bean
 	public WebClient webClient() {
+		
 		return WebClient.builder()
 				.baseUrl("http://localhost:8080")
+//				.defaultHeaders(h -> h.setBasicAuth("username","password"))
+//				.filter((clientRequest, exchangeFunction) -> sessionToken(clientRequest, exchangeFunction))
+				.filter(this::sessionToken)
 				.build();
+	}
+	
+	private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex){
+		System.out.println("generating session token");
+		ClientRequest clientRequest = ClientRequest.from(request).headers(h -> h.setBearerAuth("some-lengthy-jwt")).build();
+		return ex.exchange(clientRequest);
 	}
 }
