78172ad2a47b1931fcdbcaaaae2966a27bde4afd
kshneo0 <kshneo@gmail.com>
2022-04-09 10:05:44 +0900

    35. Path Based Routing Config

--- webflux-demo/src/main/java/com/ksh/webfluxdemo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/ksh/webfluxdemo/config/RouterConfig.java
@@ -20,7 +20,27 @@ public class RouterConfig {
 	
 	@Autowired
 	private RequestHandler requestHandler;
+
+	@Bean
+	public RouterFunction<ServerResponse> highLevelRouter() {
+		return RouterFunctions.route()
+				.path("router", this::serverResponseRouterFunction)
+				.build();
+	}
 	
+//	@Bean
+	private RouterFunction<ServerResponse> serverResponseRouterFunction() {
+		return RouterFunctions.route()
+				.GET("square/{input}",requestHandler::squareHandler)
+				.GET("table/{input}",requestHandler::tableHandler)
+				.GET("table/{input}/stream",requestHandler::tableStreamHandler)
+				.POST("multiply",requestHandler::multiplyHandler)
+				.GET("square/{input}/validation",requestHandler::squareHandlerWithValidation)
+				.onError(InputValidationException.class, exceptionHandler())
+				.build();
+	}
+	
+/*	
 	@Bean
 	public RouterFunction<ServerResponse> serverResponseRouterFunction() {
 		return RouterFunctions.route()
@@ -32,7 +52,7 @@ public class RouterConfig {
 				.onError(InputValidationException.class, exceptionHandler())
 				.build();
 	}
-	
+*/	
 	private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler(){
 		return (err, req) -> {
 			InputValidationException ex = (InputValidationException)err;
