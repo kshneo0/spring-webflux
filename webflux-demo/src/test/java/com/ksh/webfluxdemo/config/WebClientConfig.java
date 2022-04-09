7a35eb3bf30ad8663f0d4c4371cc43b6f6a41f0e
kshneo0 <kshneo@gmail.com>
2022-04-09 17:34:25 +0900

    53. Attributes

--- webflux-demo/src/test/java/com/ksh/webfluxdemo/config/WebClientConfig.java
+++ webflux-demo/src/test/java/com/ksh/webfluxdemo/config/WebClientConfig.java
@@ -22,10 +22,31 @@ public class WebClientConfig {
 				.filter(this::sessionToken)
 				.build();
 	}
-	
+/*	
 	private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex){
 		System.out.println("generating session token");
 		ClientRequest clientRequest = ClientRequest.from(request).headers(h -> h.setBearerAuth("some-lengthy-jwt")).build();
 		return ex.exchange(clientRequest);
+	}
+*/	
+	private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex){
+		//auth --> basic or oauth
+		ClientRequest clientRequest = request.attribute("auth")
+			.map(v -> v.equals("basic") ? withBasicAuth(request) : withOAuth(request))
+			.orElse(request);
+		
+		return ex.exchange(clientRequest);
+	}
+	
+	private ClientRequest withBasicAuth(ClientRequest request) {
+		return ClientRequest.from(request)
+				.headers(h -> h.setBasicAuth("username", "password"))
+				.build();						
+	}
+	
+	private ClientRequest withOAuth(ClientRequest request) {
+		return ClientRequest.from(request)
+				.headers(h -> h.setBearerAuth("some-token"))
+				.build();						
 	}
 }
