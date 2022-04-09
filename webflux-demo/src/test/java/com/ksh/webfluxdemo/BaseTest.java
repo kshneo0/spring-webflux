0421128acdaf64245c0ea6eaf69fc004e067366c
kshneo0 <kshneo@gmail.com>
2022-04-09 11:50:15 +0900

    42. GET Mono Endpoint

--- webflux-demo/src/test/java/com/ksh/webfluxdemo/BaseTest.java
+++ webflux-demo/src/test/java/com/ksh/webfluxdemo/BaseTest.java
@@ -1,13 +1,9 @@
 package com.ksh.webfluxdemo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BaseTest {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- webflux-demo/src/test/java/com/ksh/webfluxdemo/Lec01GetSingleResponseTest.java
+++ webflux-demo/src/test/java/com/ksh/webfluxdemo/Lec01GetSingleResponseTest.java
@@ -1,11 +1,26 @@
 package com.ksh.webfluxdemo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.ksh.webfluxdemo.dto.Response;
 
 public class Lec01GetSingleResponseTest extends BaseTest{
 	
 	@Autowired
 	private WebClient webClient;
+	
+	@Test
+	public void blockTest() {
+		Response response = this.webClient
+			.get()
+			.uri("/reactive-math/square/{number}", 5)
+			.retrieve()
+			.bodyToMono(Response.class)	//Mono<Response>
+			.block();
+		
+		System.out.println(response);
+	}
 
 }
