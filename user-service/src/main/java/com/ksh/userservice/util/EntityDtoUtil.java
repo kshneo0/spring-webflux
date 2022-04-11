bee5c083b04f9854c6a418bf75dffcfcbb8c80ca
kshneo0 <kshneo@gmail.com>
2022-04-12 07:09:28 +0900

    91. [User-Service] - Transaction DTO Util

--- user-service/src/main/java/com/ksh/userservice/util/EntityDtoUtil.java
+++ user-service/src/main/java/com/ksh/userservice/util/EntityDtoUtil.java
@@ -1,9 +1,15 @@
 package com.ksh.userservice.util;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.BeanUtils;
 
+import com.ksh.userservice.dto.TransactionRequestDto;
+import com.ksh.userservice.dto.TransactionResponseDto;
+import com.ksh.userservice.dto.TransactionStatus;
 import com.ksh.userservice.dto.UserDto;
 import com.ksh.userservice.entity.User;
+import com.ksh.userservice.entity.UserTransaction;
 
 public class EntityDtoUtil {
 
@@ -16,5 +22,22 @@ public class EntityDtoUtil {
 		User user = new User();
 		BeanUtils.copyProperties(dto, user);
 		return user;
+	}
+	
+	public static UserTransaction toEntity(TransactionRequestDto requestDto) {
+		UserTransaction ut = new UserTransaction();
+		ut.setUserId(requestDto.getUserId());
+		ut.setAmount(requestDto.getAmount());
+		ut.setTransactionDate(LocalDateTime.now());
+		return ut;
+	}
+	
+	public static TransactionResponseDto toDto(TransactionRequestDto requestDto, TransactionStatus status) {
+		TransactionResponseDto responseDto = new TransactionResponseDto();
+		responseDto.setAmount(requestDto.getAmount());
+		responseDto.setUserId(requestDto.getUserId());
+		responseDto.setStatus(status);
+		return responseDto;
+		
 	}
 }
