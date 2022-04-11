e5bb76f2903ff3e718e0d550f374a4e2066cfab9
kshneo0 <kshneo@gmail.com>
2022-04-12 07:01:00 +0900

    90. [User-Service] - User Balance Update - R2DBC with @Query

--- user-service/src/main/java/com/ksh/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/ksh/userservice/repository/UserRepository.java
@@ -1,12 +1,24 @@
 package com.ksh.userservice.repository;
 
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ksh.userservice.entity.User;
 
+import reactor.core.publisher.Mono;
+
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+	
+	@Modifying
+	@Query( "update users " +
+			"   set balance = balance - :amount" +
+			" where id = :userId " + 
+			"   and balance >= :amount"
+	)
+	Mono<Boolean> updateUserBalabce(int userId, int amt);
 
 
 
