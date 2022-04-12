986ce92a4e3117fbca8b76bdf9957478e2063300
kshneo0 <kshneo@gmail.com>
2022-04-12 10:50:06 +0900

    113. [Order-Service] - User Client

---
+++ order-service/src/main/java/com/ksh/orderservice/client/UserClient.java
@@ -0,0 +1,31 @@
+package com.ksh.orderservice.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.ksh.orderservice.dto.TransactionRequestDto;
+import com.ksh.orderservice.dto.TransactionResponseDto;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class UserClient {
+    private final WebClient webClient;
+
+    public UserClient(@Value("${user.service.url}") String url){
+        this.webClient = WebClient.builder()
+                .baseUrl(url)
+                .build();
+    }
+
+    public Mono<TransactionResponseDto> authorizeTransaction(TransactionRequestDto requestDto){
+        return this.webClient
+                .post()
+                .uri("transaction")
+                .bodyValue(requestDto)
+                .retrieve()
+                .bodyToMono(TransactionResponseDto.class);
+    }
+
+}
