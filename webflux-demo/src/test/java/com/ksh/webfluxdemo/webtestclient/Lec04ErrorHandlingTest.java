3d892b92667fff714da9cb0fc9e7d99b1fb5cbee
kshneo0 <kshneo@gmail.com>
2022-04-12 19:13:14 +0900

    146. Controller Advice

---
+++ webflux-demo/src/test/java/com/ksh/webfluxdemo/webtestclient/Lec04ErrorHandlingTest.java
@@ -0,0 +1,40 @@
+package com.ksh.webfluxdemo.webtestclient;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.ksh.webfluxdemo.controller.ReactiveMathValidationController;
+import com.ksh.webfluxdemo.dto.Response;
+import com.ksh.webfluxdemo.service.ReactiveMathService;
+
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(ReactiveMathValidationController.class)
+public class Lec04ErrorHandlingTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @MockBean
+    private ReactiveMathService reactiveMathService;
+
+    @Test
+    public void errorHandlingTest(){
+        Mockito.when(reactiveMathService.findSquare(Mockito.anyInt())).thenReturn(Mono.just(new Response(1)));
+
+        this.client
+               .get()
+                .uri("/reactive-math/square/{number}/throw", 5) // 10-20
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("allowed range is 10 - 20")
+                .jsonPath("$.errorCode").isEqualTo(100)
+                .jsonPath("$.input").isEqualTo(5);
+    }
+
+}
