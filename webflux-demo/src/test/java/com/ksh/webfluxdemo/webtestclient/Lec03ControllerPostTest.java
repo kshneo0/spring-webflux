1016591deb0d2e7bb9311d12ce9d67670fb4180a
kshneo0 <kshneo@gmail.com>
2022-04-12 19:03:10 +0900

    145. Post Test

---
+++ webflux-demo/src/test/java/com/ksh/webfluxdemo/webtestclient/Lec03ControllerPostTest.java
@@ -0,0 +1,43 @@
+package com.ksh.webfluxdemo.webtestclient;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.ksh.webfluxdemo.controller.ReactiveMathController;
+import com.ksh.webfluxdemo.dto.MultiplyRequestDto;
+import com.ksh.webfluxdemo.dto.Response;
+import com.ksh.webfluxdemo.service.ReactiveMathService;
+
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(ReactiveMathController.class)
+public class Lec03ControllerPostTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @MockBean
+    private ReactiveMathService reactiveMathService;
+
+    @Test
+    public void postTest(){
+        Mockito.when(reactiveMathService.multiply(Mockito.any())).thenReturn(Mono.just(new Response(1)));
+
+        this.client
+                .post()
+                .uri("/reactive-math/multiply")
+                .accept(MediaType.APPLICATION_JSON)
+                .headers(h -> h.setBasicAuth("username", "password"))
+                .headers(h -> h.set("somekey", "somevalue"))
+                .bodyValue(new MultiplyRequestDto())
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+
+}
